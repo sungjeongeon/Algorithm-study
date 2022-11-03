f87ea863d5b2f741992b653b3d621a4738d5678b
sje1942 <sje1942@naver.com>
2022-11-03 23:08:13 +0900

    221103

---
+++ TestA/src/Baekjoon/Baekjoon_14499_주사위굴리기.java
@@ -0,0 +1,34 @@
+package Baekjoon;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Baekjoon_14499_주사위굴리기 {
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = null;
+		
+		st = new StringTokenizer(br.readLine());
+		int N = Integer.parseInt(st.nextToken());
+		int M = Integer.parseInt(st.nextToken());
+		int x = Integer.parseInt(st.nextToken());
+		int y = Integer.parseInt(st.nextToken());
+		int k = Integer.parseInt(st.nextToken());
+		
+		int map[][] = new int[N][M];
+		for(int i=0; i<N; i++) {
+			st = new StringTokenizer(br.readLine());
+			for(int j=0; j<M; j++) {
+				map[i][j] = Integer.parseInt(st.nextToken());
+			}
+		}
+		
+		int cmd[] = new int[k];
+		st = new StringTokenizer(br.readLine());
+		for(int i=0; i<k; i++) {
+			cmd[i] = Integer.parseInt(st.nextToken());
+		}
+	}
+}
